1681fa213b1bb06f58db9a38abb93bf3304305db
Irga <irgantaraa@gmail.com>
2024-11-17 21:01:40 +0700

    HadirApp

--- src/test/java/com/juaracoding/TestRunner.java
+++ src/test/java/com/juaracoding/TestRunner.java
@@ -3,7 +3,7 @@ package com.juaracoding;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 //"src/main/resources/features/IzinPulangCepat.feature","src/main/resources/features/Cuti.feature"src/main/resources/features/IzinPulangCepat.feature","src/main/resources/features/Cuti.feature",src/main/resources/features/SakitMobile.feature
-@CucumberOptions(features = {"src/main/resources/features/Cuti.feature"},
+@CucumberOptions(features = {"src/main/resources/features/IzinPulangCepat.feature","src/main/resources/features/Cuti.feature","src/main/resources/features/Jabatan.feature","src/main/resources/features/SakitMobile.feature","src/main/resources/features/CutiMobile.feature"},
         glue = "com.juaracoding",
         plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
